3b459a2b71c3741a305d24a24bf98adfccd595f4
aphrodite <hugohooverwang@126.com>
2018-11-05 17:10:39 +0800

    uppdatera utkast

--- hypoxic-hypoxia.html.pm
+++ hypoxic-hypoxia.html.pm
@@ -6,6 +6,74 @@
 ◊(define-meta desc "")
 
 ◊section{
-  ◊article{}
-  ◊aside{◊em{◊(hash-ref metas 'desc)}}
+  ◊h1{Hypoxic hypoxia}
+  ◊article{
+    ◊h2{koncept}
+      ◊p{Hypoxic hypoxia also called arterial hypoxia is a result of insufficient oxygen available to the lungs.
+         A blocked airway, drowning or a reduction in partial pressure (high altitude above 3048 meters) are examples of how lungs can be deprived of oxygen.
+          Some medical examples are abnormal pulmonary function or respiratory obstruction.
+           Hypoxic hypoxia is seen in patients suffering from chronic obstructive pulmonary diseases (COPD), neuromuscular diseases or interstitial lung disease.}
+      ◊p{When your body doesn't have enough oxygen, you could get hypoxemia or hypoxia. These are dangerous conditions. Without oxygen, your brain, liver, and other organs can be damaged just minutes after symptoms start.}
+
+      ◊p{Hypoxemia (low oxygen in your blood) can cause hypoxia (low oxygen in your tissues) when your blood doesn't carry enough oxygen to your tissues to meet your body's needs. The word hypoxia is sometimes used to describe both problems.}
+
+    ◊h2{Symptoms}
+      ◊p{Although they can vary from person to person, the most common hypoxia symptoms are:}
+      ◊ul{
+        ◊li{Cyanosis, Changes in the color of your skin, ranging from blue to cherry red}
+        ◊li{Headache}
+        ◊li{Increased reaction time}
+        ◊li{Impaired judgment time}
+        ◊li{Euphoria}
+        ◊li{Visual impairment}
+        ◊li{Drowsiness}
+        ◊li{Lightheaded or dizzy sensation}
+        ◊li{Tingling in fingers and toes}
+        ◊li{Numbness}
+        ◊li{Confusion}
+        ◊li{Cough}
+        ◊li{Fast heart rate}
+        ◊li{Rapid breathing}
+        ◊li{Shortness of breath}
+        ◊li{Slow heart rate}
+        ◊li{Sweating}
+        ◊li{Wheezing}
+      }
+
+    ◊h2{Diagnosis}
+      ◊p{Oxygen therapy is very beneficial.}
+
+    ◊h2{How It's Treated}
+      ◊p{You'll need to go to the hospital to get treatment for hypoxia and to keep a check on your oxygen level.}
+      ◊p{The most important thing is to get more oxygen into your body. You'll receive it through a small plug in your nose or through a mask that covers your nose and mouth. For many people, this is enough to bring your oxygen level up to normal.}
+      ◊p{An inhaler or asthma medicine by mouth may make breathing easier. If these don't help, the doctor might try giving you medicine through a vein in your arm (an IV). You might need steroid drugs for a short time to shrink inflammation in your lungs or antibiotics to treat an underlying infection.}
+      ◊p{When your life is in danger and other treatments aren't working, you may need a machine to help you breathe.}
+
+    ◊h2{Causes of Hypoxia}
+      ◊p{A severe asthma attack, or flare, can cause hypoxia in adults and kids. During an attack, your airways narrow, making it hard to get air into your lungs. Coughing to clear your lungs uses even more oxygen and can make symptoms worse.}
+      ◊p{Hypoxia can also result from lung damage due to trauma.}
+      ◊p{Other things can cause hypoxia include:}
+      ◊ul{
+        ◊li{Lung diseases such as chronic obstructive pulmonary disease (COPD), emphysema, bronchitis, pneumonia, and pulmonary edema (fluid in the lungs)}
+        ◊li{Strong pain medicines and other drugs that hold back breathing}
+        ◊li{Heart problems}
+        ◊li{Anemia (a low number of red blood cells, which carry oxygen)}
+        ◊li{Cyanide poisoning (Cyanide is a chemical used to make plastics and other products.)}
+      }
+
+    ◊h2{Preventing Hypoxia}
+      ◊p{The best way to prevent hypoxia is to keep your asthma under control, every day. Stick with your asthma treatment plan.}
+      ◊ul{
+        ◊li{Take your medicine to help prevent flares and the need to use your rescue inhaler.}
+        ◊li{Eat right and stay active.}
+        ◊li{Know your asthma triggers, and find ways to avoid them.}
+      }
+      ◊p{Work with your doctor to come up with an action plan for asthma attacks, so you know what to do when you have trouble breathing.}
+  }
+  ◊aside{
+    ◊em{◊(hash-ref metas 'desc)}
+    ◊ol{
+      ◊li{◊link["https://www.webmd.com/asthma/guide/hypoxia-hypoxemia#1"]{Hypoxia and Hypoxemia. WebMD Medical Reference Reviewed by Carol DerSarkissian on July 30, 2018}}
+    }
+  }
 }
